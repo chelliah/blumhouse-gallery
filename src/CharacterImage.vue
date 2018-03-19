dcaecc8d8e1f80a860e4280e9d463f11efa1f9a4
Aron Thomas <aron.c.thomas@gmail.com>
2018-03-18 20:01:35 -0500

    fix mousemoveanimations'

--- src/CharacterImage.vue
+++ src/CharacterImage.vue
@@ -35,12 +35,14 @@
                 </g>
             </g>
         </g>
-        <g id="traces">
+        <g
+
+            :transform="`${ (transformData['traces']) ? transformData['traces'].transform : 'translate(0,0)'}`"
+            id="traces">
             <path
                 v-for="(value, key) in tracePathsTweened"
                 :key="key"
                 class="cls-4"
-                :transform="`${ (transformData['trace-path-' + key]) ? transformData['trace-path-' + key].transform : 'translate(0,0)'}`"
                 :id="`trace-path-${key}`"
                 :d="value"/>
             <!-- <path id="trace-path" class="cls-4" v-bind:d="tracePathsTweened" /> -->
@@ -99,7 +101,8 @@
             generateTransformData: function(paths) {
                 let transformData = {}
 
-                const image = document.getElementById("imageclip")
+                const image = document.getElementById("imageclip");
+                const traces = document.getElementById("traces");
 
                 if (image) {
                     transformData['imageclip'] = {
@@ -108,38 +111,44 @@
                     }
                 }
 
-                paths.forEach((path, index) => {
-                    let pathNode = document.getElementById(`trace-path-${index}`)
-                    if(pathNode) {
-                        transformData[`trace-path-${index}`] = {
-                            coordinates: pathNode.getBoundingClientRect(),
-                            transform: 'translate(0, 0)'
-                        }
+                if (traces) {
+                    transformData['traces'] = {
+                        coordinates: traces.getBoundingClientRect(),
+                        transform: 'translate(0, 0)'
                     }
-                })
-                console.log(transformData)
+                }
+
                 return transformData;
 
             },
             handleMouseMove: function(e) {
-                // console.log(this.transformData);
                 const yPos = e.pageY;
                 const xPos = e.pageX;
                 const updatedTransformData = this.transformData;
-                const MAX_DISTANCE = 25;
+                const MAX_DISTANCE = 15;
 
-                if(updatedTransformData) {
-                    Object.keys(updatedTransformData).map((dataKey, index) => {
-                        const coordinates = updatedTransformData[dataKey].coordinates
-                        let elX = Math.floor(coordinates.x + (coordinates.width/2))
-                        let elY = Math.floor(coordinates.y + coordinates.height/2)
-                        let xDistance = Math.sqrt(elX - xPos)
-                        let yDistance = Math.sqrt(elY - yPos)
-                        updatedTransformData[dataKey].transform =
-                            `translate( ${Math.abs(xDistance) > 10 ? (MAX_DISTANCE/xDistance) : '0'} , ${Math.abs(yDistance) > 10 ? (MAX_DISTANCE/yDistance) : '0'})`
-                        // console.log(coordinates)
-                    })
+                if(!updatedTransformData) return false;
+
+                if (updatedTransformData['imageclip']) {
+                    let coordinates = updatedTransformData['imageclip'].coordinates
+
+                    let elX = Math.floor(coordinates.x + (coordinates.width/2))
+                    let elY = Math.floor(coordinates.y + coordinates.height/2)
+                    let xDistance = (elX - xPos)/(document.documentElement.clientWidth/MAX_DISTANCE)
+                    let yDistance = (elY - yPos)/(document.documentElement.clientWidth/MAX_DISTANCE)
+                    updatedTransformData['imageclip'].transform = `translate(${ xDistance }, ${ yDistance })`
                 }
+
+                if (updatedTransformData['traces']) {
+                    let coordinates = updatedTransformData['traces'].coordinates
+
+                    let elX = Math.floor(coordinates.x + (coordinates.width/2))
+                    let elY = Math.floor(coordinates.y + coordinates.height/2)
+                    let xDistance = (elX - xPos)/(document.documentElement.clientWidth/MAX_DISTANCE)
+                    let yDistance = (elY - yPos)/(document.documentElement.clientWidth/MAX_DISTANCE)
+                    updatedTransformData['traces'].transform = `translate(${ -xDistance }, ${ -yDistance })`
+                }
+
 
                 this.transformData = updatedTransformData
 
